58bf177bcf7ca331514d788460cf8906377a9040
273* <keita.y310@icloud.com>
2026-05-01 23:47:59 +0900

    feat: ジャーナル一覧画面を実装

---
+++ src/app/(journal)/journal.tsx
@@ -0,0 +1,85 @@
+import { PlatformColor, useColorScheme, View } from "react-native";
+
+import { Host, Text } from "@expo/ui/swift-ui";
+import { font, padding } from "@expo/ui/swift-ui/modifiers";
+import { Stack } from "expo-router";
+
+/**
+ * ジャーナル一覧画面
+ */
+export default function JournalScreen() {
+  const colorScheme = useColorScheme();
+  const titleColor = colorScheme === "dark" ? "#ffffff" : "#000000";
+
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          title: "Journal Title",
+          headerTitleStyle: {
+            color: titleColor,
+          },
+          headerShown: true,
+          headerLargeTitleEnabled: true,
+          headerBackButtonDisplayMode: "minimal",
+          unstable_headerRightItems: () => [
+            {
+              type: "menu",
+              label: "Options",
+              icon: {
+                type: "sfSymbol",
+                name: "ellipsis",
+              },
+              menu: {
+                items: [
+                  {
+                    type: "submenu",
+                    label: "Sort",
+                    icon: {
+                      type: "sfSymbol",
+                      name: "arrow.up.arrow.down",
+                    },
+                    items: [
+                      {
+                        type: "action",
+                        label: "hoge",
+                        onPress: () => {
+                          // Do something
+                        },
+                      },
+                      {
+                        type: "action",
+                        label: "fuga",
+                        onPress: () => {
+                          // Do something
+                        },
+                      },
+                    ],
+                  },
+                ],
+              },
+            },
+          ],
+        }}
+      />
+      <View style={{ flex: 1 }}>
+        <Host
+          style={{
+            flex: 1,
+            backgroundColor: PlatformColor("systemBackground"),
+          }}
+          useViewportSizeMeasurement
+        >
+          <Text
+            modifiers={[
+              font({ size: 28, weight: "bold" }),
+              padding({ bottom: 8 }),
+            ]}
+          >
+            ジャーナル
+          </Text>
+        </Host>
+      </View>
+    </>
+  );
+}
