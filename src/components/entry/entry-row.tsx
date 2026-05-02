42ef2884c86e70845def521fc02f1173cae4e1b6
273* <keita.y310@icloud.com>
2026-05-02 17:52:47 +0900

    fix: listでボタンを使用するよう修正

--- src/components/entry/entry-row.tsx
+++ src/components/entry/entry-row.tsx
@@ -1,12 +1,7 @@
 import { PlatformColor } from "react-native";
 
-import { HStack, Image, Spacer, Text, VStack } from "@expo/ui/swift-ui";
-import {
-  font,
-  foregroundStyle,
-  lineLimit,
-  onTapGesture,
-} from "@expo/ui/swift-ui/modifiers";
+import { Button, HStack, Image, Spacer, Text, VStack } from "@expo/ui/swift-ui";
+import { font, foregroundStyle, lineLimit } from "@expo/ui/swift-ui/modifiers";
 import { useRouter } from "expo-router";
 
 import { Entry } from "@/mocks/entries";
@@ -19,30 +14,29 @@ const secondary = foregroundStyle({ type: "hierarchical", style: "secondary" });
 export function EntryRow({ entry }: { entry: Entry }) {
   const router = useRouter();
   return (
-    <VStack
-      alignment="leading"
-      spacing={4}
-      modifiers={[
-        onTapGesture(() => router.push(`/(journal)/entry/${entry.id}`)),
-      ]}
+    <Button
+      modifiers={[foregroundStyle({ type: "hierarchical", style: "primary" })]}
+      onPress={() => router.push(`/(journal)/entry/${entry.id}`)}
     >
-      <HStack alignment="center" spacing={6}>
-        <Text modifiers={[font({ size: 12 }), secondary]}>{entry.date}</Text>
-        <Spacer />
-        {entry.bookmark && (
-          <Image
-            systemName="bookmark.fill"
-            size={12}
-            color={PlatformColor("systemIndigo")}
-          />
-        )}
-      </HStack>
-      <Text modifiers={[font({ size: 16, weight: "bold" })]}>
-        {entry.title}
-      </Text>
-      <Text modifiers={[font({ size: 14 }), secondary, lineLimit(2)]}>
-        {entry.preview}
-      </Text>
-    </VStack>
+      <VStack alignment="leading" spacing={4}>
+        <HStack alignment="center" spacing={6}>
+          <Text modifiers={[font({ size: 12 }), secondary]}>{entry.date}</Text>
+          <Spacer />
+          {entry.bookmark && (
+            <Image
+              systemName="bookmark.fill"
+              size={12}
+              color={PlatformColor("systemIndigo")}
+            />
+          )}
+        </HStack>
+        <Text modifiers={[font({ size: 16, weight: "bold" })]}>
+          {entry.title}
+        </Text>
+        <Text modifiers={[font({ size: 14 }), secondary, lineLimit(2)]}>
+          {entry.preview}
+        </Text>
+      </VStack>
+    </Button>
   );
 }
