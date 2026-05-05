df85d7cdcea3abd2e2da6901125b9d11d48e5cd4
273* <keita.y310@icloud.com>
2026-05-06 01:16:03 +0900

    feat: dbスキーマを実装

---
+++ src/db/schemas/journals.ts
@@ -0,0 +1,31 @@
+import { relations } from "drizzle-orm";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import type { SFSymbol } from "expo-symbols";
+
+import { entries } from "./entries";
+import { fields } from "./fields";
+
+/**
+ * ジャーナルテーブル
+ */
+export const journals = sqliteTable("journals", {
+  id: text().primaryKey(),
+  name: text().notNull(),
+  /** SFSymbol名 */
+  icon: text().notNull().$type<SFSymbol>(),
+  /** HEXカラーコード */
+  color: text().notNull(),
+  createdAt: integer()
+    .notNull()
+    .$defaultFn(() => Date.now()),
+  updatedAt: integer()
+    .notNull()
+    .$defaultFn(() => Date.now()),
+});
+
+export const journalsRelations = relations(journals, ({ many }) => ({
+  fields: many(fields),
+  entries: many(entries),
+}));
+
+export type JournalObj = typeof journals.$inferSelect;
