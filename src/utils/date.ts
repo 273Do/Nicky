e4b47c3e75ce306c3e49e90230c1e99bd3e66325
273* <keita.y310@icloud.com>
2026-05-10 15:20:37 +0900

    feat: 時間のフォーマット関数を実装

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -13,6 +13,18 @@ export const formatDate = (date: Date | number): string => {
 };
 
 /**
+ * 時刻を "13:05" などロケールに応じた形式でフォーマットする関数
+ * @param date フォーマットする日付
+ * @returns フォーマットされた時刻文字列
+ */
+export const formatTime = (date: Date | number): string => {
+  return new Intl.DateTimeFormat(undefined, {
+    hour: "numeric",
+    minute: "2-digit",
+  }).format(date);
+};
+
+/**
  * 年月を "2026年5月" / "May 2026" などロケールに応じた形式でフォーマットする関数
  * @param date フォーマットする日付
  * @returns フォーマットされた年月文字列
